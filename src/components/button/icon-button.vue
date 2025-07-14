ccf0becb80b1c0fec214c411a33f780042fc733d
CarloBattista <carlitobatti@gmail.com>
2025-07-14 20:12:33 +0200

    feat(button): add icon-button component and update loading animation
    - Replace dot loader animation with spinning circle animation in button-lg
    - Create new icon-button component with similar styling and loading state
    - Update Home view to demonstrate both button components

--- src/components/button/icon-button.vue
+++ src/components/button/icon-button.vue
@@ -3,30 +3,25 @@
     tabindex="0"
     :type="type"
     :disabled="disabled"
-    class="btn-lg min-h-10 max-h-16 flex gap-2 items-center justify-center"
+    class="icon-btn aspect-square flex items-center justify-center"
     :class="'size-' + size + ' variant-' + variant"
   >
-    <div v-if="loading" class="loader"></div>
-    <div v-if="!loading && leftIcon" class="btn-icon h-full flex items-center justify-center">
-      <component :is="leftIcon" />
+    <div v-if="!loading && icon" class="btn-icon h-full aspect-square flex items-center justify-center">
+      <component :is="icon" />
     </div>
-    <span v-if="!loading && label" class="btn-label font-semibold">{{ label }}</span>
-    <div v-if="!loading && rightIcon" class="btn-icon h-full flex items-center justify-center">
-      <component :is="rightIcon" />
-    </div>
+    <div v-else-if="loading" class="loader"></div>
   </button>
 </template>
 
 <script>
 // ICONS
-import { Mail, ArrowRight } from 'lucide-vue-next';
+import { Mail } from 'lucide-vue-next';
 
 export default {
-  name: 'button-lg',
+  name: 'icon-button',
   components: {
     // ICONS
     Mail,
-    ArrowRight,
   },
   props: {
     type: {
@@ -41,9 +36,7 @@ export default {
       type: String,
       default: 'primary',
     },
-    label: String,
-    leftIcon: String,
-    rightIcon: String,
+    icon: String,
     loading: {
       type: Boolean,
       default: false,
@@ -57,7 +50,7 @@ export default {
 </script>
 
 <style scoped>
-.btn-lg {
+.icon-btn {
   outline-style: solid;
   outline-width: 1px;
   outline-offset: 0px;
@@ -67,91 +60,69 @@ export default {
   transition-timing-function: ease;
 }
 
-.btn-lg:not(:disabled) {
+.icon-btn:not(:disabled) {
   cursor: pointer;
 }
 
 /* Sizes */
-.btn-lg.size-sm {
+.icon-btn.size-sm {
   height: 40px;
-  padding: 10px 18px;
 }
 
-.btn-lg.size-sm .btn-icon,
-.btn-lg.size-md .btn-icon {
+.icon-btn.size-sm .btn-icon,
+.icon-btn.size-md .btn-icon {
   width: 18px;
   height: 18px;
 }
 
-.btn-lg.size-sm .btn-label,
-.btn-lg.size-md .btn-label {
-  font-size: 14px;
-}
-
-.btn-lg.size-md {
+.icon-btn.size-md {
   height: 44px;
-  padding: 12px 20px;
 }
 
-.btn-lg.size-lg {
+.icon-btn.size-lg {
   height: 52px;
-  padding: 14px 22px;
 }
 
-.btn-lg.size-lg .btn-label {
-  font-size: 1rem;
-}
-
-.btn-lg.size-lg .btn-icon,
-.btn-lg.size-xl .btn-icon {
+.icon-btn.size-lg .btn-icon,
+.icon-btn.size-xl .btn-icon {
   width: 20px;
   height: 20px;
 }
 
-.btn-lg.size-xl {
+.icon-btn.size-xl {
   height: 56px;
-  padding: 16px 24px;
 }
 
-.btn-lg.size-xl .btn-label {
-  font-size: 1.125rem;
-}
-
-.btn-lg.size-2xl {
+.icon-btn.size-2xl {
   height: 64px;
-  padding: 18px 26px;
 }
 
-.btn-lg.size-2xl .btn-icon {
+.icon-btn.size-2xl .btn-icon {
   width: 24px;
   height: 24px;
 }
 
-.btn-lg.size-2xl .btn-label {
-  font-size: 1.25rem;
-}
-
 /* Primary */
-.btn-lg.variant-primary {
+.icon-btn.variant-primary {
   outline-color: rgba(103, 110, 118, 0.16);
   background-color: var(--primary---black);
   color: var(--primary---white);
 }
 
-.btn-lg.variant-primary:hover {
+.icon-btn.variant-primary:hover {
   outline-color: rgba(103, 110, 118, 0.08);
   background-color: var(--grey--1000);
   color: var(--primary---white);
 }
 
-.btn-lg.variant-primary:focus {
+.icon-btn.variant-primary:focus {
   outline-width: 4px;
   outline-color: rgba(103, 110, 118, 0.16);
   background-color: var(--primary---black);
   color: var(--primary---white);
 }
 
-.btn-lg.variant-primary:disabled {
+.icon-btn.variant-primary:disabled {
   outline-color: rgba(103, 110, 118, 0.08);
   background-color: var(--grey--100);
   color: var(--grey--300);
@@ -163,52 +134,52 @@ export default {
 }
 
 /* Secondary Color */
-.btn-lg.variant-secondary-color {
+.icon-btn.variant-secondary-color {
   outline-color: rgba(103, 110, 118, 0.16);
   background-color: var(--primary---white);
   color: var(--primary---black);
 }
 
-.btn-lg.variant-secondary-color:hover {
+.icon-btn.variant-secondary-color:hover {
   outline-color: rgba(0, 0, 0, 0.64);
   background-color: var(--grey--100);
   color: var(--primary---black);
 }
 
-.btn-lg.variant-secondary-color:focus {
+.icon-btn.variant-secondary-color:focus {
   outline-width: 4px;
   outline-color: rgba(103, 110, 118, 0.16);
   background-color: var(--primary---white);
   color: var(--primary---black);
 }
 
-.btn-lg.variant-secondary-color:disabled {
+.icon-btn.variant-secondary-color:disabled {
   outline-color: rgba(103, 110, 118, 0.08);
   background-color: var(--primary---white);
   color: var(--grey--300);
 }
 
 /* Secondary Grey */
-.btn-lg.variant-secondary-grey {
+.icon-btn.variant-secondary-grey {
   outline-color: rgba(103, 110, 118, 0.16);
   background-color: var(--grey--50);
   color: var(--primary---black);
 }
 
-.btn-lg.variant-secondary-grey:hover {
+.icon-btn.variant-secondary-grey:hover {
   outline-color: rgba(0, 0, 0, 0.64);
   background-color: var(--grey--200);
   color: var(--primary---black);
 }
 
-.btn-lg.variant-secondary-grey:focus {
+.icon-btn.variant-secondary-grey:focus {
   outline-width: 4px;
   outline-color: rgba(103, 110, 118, 0.16);
   background-color: var(--grey--50);
   color: var(--primary---black);
 }
 
-.btn-lg.variant-secondary-grey:disabled {
+.icon-btn.variant-secondary-grey:disabled {
   outline-color: rgba(103, 110, 118, 0.08);
   background-color: var(--grey--50);
   color: var(--grey--300);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,20 @@
 <template>
-  <div></div>
+  <div class="m-10">
+    <buttonLg type="button" size="md" variant="secondary-grey" leftIcon="Mail" rightIcon="Mail" label="button" :loading="true" :disabled="false" />
+    <iconButton type="button" size="md" variant="secondary-grey" icon="Mail" :loading="true" :disabled="false" />
+  </div>
 </template>
 
 <script>
+import buttonLg from '../components/button/button-lg.vue';
+import iconButton from '../components/button/icon-button.vue';
+
 export default {
   name: 'Home',
+  components: {
+    buttonLg,
+    iconButton,
+  },
 };
 </script>
 
